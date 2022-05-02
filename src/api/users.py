7f35c3485f0bb2645f212044ba554e101b4c87a5
davidttpb <dalvarezpb@gmail.com>
2022-05-01 22:05:36 -0500

    adding new tests, files, and configuring Dockerfile.prod

--- src/api/users.py
+++ src/api/users.py
@@ -1,13 +1,23 @@
-from flask import Blueprint, request
-from flask_restx import Api, Resource, fields  # updated
+# src/api/users.py
 
-from src import db
-from src.api.models import User
+
+from flask import Blueprint, request
+from flask_restx import Api, Resource, fields
+
+# new
+from src.api.crud import (  # isort:skip
+    get_all_users,
+    get_user_by_email,
+    add_user,
+    get_user_by_id,
+    update_user,
+    delete_user,
+)
 
 users_blueprint = Blueprint("users", __name__)
 api = Api(users_blueprint)
 
-# new
+
 user = api.model(
     "User",
     {
@@ -20,36 +30,32 @@ user = api.model(
 
 
 class UsersList(Resource):
-    @api.expect(user, validate=True)  # new
+    @api.marshal_with(user, as_list=True)
+    def get(self):
+        return get_all_users(), 200  # updated
+
+    @api.expect(user, validate=True)
     def post(self):
         post_data = request.get_json()
         username = post_data.get("username")
         email = post_data.get("email")
         response_object = {}
 
-        user = User.query.filter_by(email=email).first()
+        user = get_user_by_email(email)  # updated
         if user:
             response_object["message"] = "Sorry. That email already exists."
             return response_object, 400
 
-        db.session.add(User(username=username, email=email))
-        db.session.commit()
+        add_user(username, email)  # new
 
         response_object["message"] = f"{email} was added!"
         return response_object, 201
-
-    @api.marshal_with(user, as_list=True)
-    def get(self):
-        return User.query.all(), 200
-
-
-api.add_resource(UsersList, "/users")
 
 
 class Users(Resource):
     @api.marshal_with(user)
     def get(self, user_id):
-        user = User.query.filter_by(id=user_id).first()
+        user = get_user_by_id(user_id)  # updated
         if not user:
             api.abort(404, f"User {user_id} does not exist")
         return user, 200
@@ -61,51 +67,31 @@ class Users(Resource):
         email = post_data.get("email")
         response_object = {}
 
-        user = User.query.filter_by(id=user_id).first()
+        user = get_user_by_id(user_id)  # updated
         if not user:
             api.abort(404, f"User {user_id} does not exist")
 
-        user.username = username
-        user.email = email
-        db.session.commit()
+        if get_user_by_email(email):  # updated
+            response_object["message"] = "Sorry. That email already exists."
+            return response_object, 400
+
+        update_user(user, username, email)  # new
 
         response_object["message"] = f"{user.id} was updated!"
         return response_object, 200
 
     def delete(self, user_id):
         response_object = {}
-        user = User.query.filter_by(id=user_id).first()
+        user = get_user_by_id(user_id)  # updated
 
         if not user:
             api.abort(404, f"User {user_id} does not exist")
 
-        db.session.delete(user)
-        db.session.commit()
+        delete_user(user)  # new
 
         response_object["message"] = f"{user.email} was removed!"
         return response_object, 200
 
-    @api.expect(user, validate=True)
-    def put(self, user_id):
-        post_data = request.get_json()
-        username = post_data.get("username")
-        email = post_data.get("email")
-        response_object = {}
 
-        user = User.query.filter_by(id=user_id).first()
-        if not user:
-            api.abort(404, f"User {user_id} does not exist")
-
-        if User.query.filter_by(email=email).first():
-            response_object["message"] = "Sorry. That email already exists."
-            return response_object, 400
-
-        user.username = username
-        user.email = email
-        db.session.commit()
-
-        response_object["message"] = f"{user.id} was updated!"
-        return response_object, 200
-
-
+api.add_resource(UsersList, "/users")
 api.add_resource(Users, "/users/<int:user_id>")
